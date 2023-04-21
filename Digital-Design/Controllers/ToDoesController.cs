580db0fca01bf159e5c1249a99ba3b9faa00ea06
karimhassan1995 <karimelmorsy95@gmail.com>
2023-04-21 21:03:11 +0200

    fix edit view

--- Digital-Design/Controllers/ToDoesController.cs
+++ Digital-Design/Controllers/ToDoesController.cs
@@ -86,6 +86,8 @@ namespace Digital_Design.Controllers
                 return NotFound();
             }
             ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", toDo.AppUserId);
+            ViewData["todo-id"] = id;
+            ViewData["userid"] = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
             return View(toDo);
         }
 
@@ -94,7 +96,7 @@ namespace Digital_Design.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ToDoTitle,ToDoDescription,AppUserId")] ToDo toDo)
+        public async Task<IActionResult> Edit(int id, [Bind("ToDoId,ToDoTitle,ToDoDescription,AppUserId")] ToDo toDo)
         {
             if (id != toDo.ToDoId)
             {
